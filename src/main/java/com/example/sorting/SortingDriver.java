4366b9447990731dd10a8e07df4e2ab24737e1d3
Motley2 <Wil2242027@maricopa.edu>
2024-11-26 15:10:38 -0700

    Implemented gnomeSort, cocktailShakerSort, shellSort

--- src/main/java/com/example/sorting/SortingDriver.java
+++ src/main/java/com/example/sorting/SortingDriver.java
@@ -5,61 +5,61 @@ import java.util.stream.IntStream;
 
 public class SortingDriver {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        // Generate array of random Integers
-        Integer[] arr = IntStream.generate(() -> (int) (Math.random() * 50000))
-                                 .boxed()
-                                 .limit(1000)
-                                 .toArray(Integer[]::new);
+            // Generate array of random Integers
+            Integer[] arr = IntStream.generate(() -> (int) (Math.random() * 50000))
+                    .boxed()
+                    .limit(1000)
+                    .toArray(Integer[]::new);
 
-        Integer[] copy1 = copyArray(arr);
-        Integer[] copy2 = copyArray(arr);
-        Integer[] copy3 = copyArray(arr);
+            Integer[] copy1 = copyArray(arr);
+            Integer[] copy2 = copyArray(arr);
+            Integer[] copy3 = copyArray(arr);
 
-        System.out.println("\nUnsorted data...");
-        printFirstTenOfArray(arr);
+            System.out.println("\nUnsorted data...");
+            printFirstTenOfArray(arr);
 
-        System.out.println("\nGnome Sort...");
-        SortingUtility.gnomeSort(copy1);
-        printFirstTenOfArray(copy1);
+            System.out.println("\nGnome Sort...");
+            SortingUtility.gnomeSort(copy1);
+            printFirstTenOfArray(copy1);
 
-        System.out.println("\nCocktail Shaker Sort...");
-        SortingUtility.cocktailShakerSort(copy2);
-        printFirstTenOfArray(copy2);
+            System.out.println("\nCocktail Shaker Sort...");
+            SortingUtility.cocktailShakerSort(copy2);
+            printFirstTenOfArray(copy2);
 
-        System.out.println("\nShell Sort...");
-        SortingUtility.shellSort(copy3);
-        printFirstTenOfArray(copy3);
+            System.out.println("\nShell Sort...");
+            SortingUtility.shellSort(copy3);
+            printFirstTenOfArray(copy3);
 
-    }
-
-    private static <T> void printFirstTenOfArray(T[] obj) {
-
-        if (obj != null) {
-
-            int counter = 1;
-            System.out.println("\nFirst 10 of Array...");
-
-            for (T i : obj) {
-                System.out.println(counter + ": " + i.toString());
-
-                if (counter >= 10) {
-                    break;
-                }
-                counter++;
-            }
-
-        } else {
-            System.out.println("Array is null.");
         }
+
+        private static <T> void printFirstTenOfArray(T[] obj) {
+
+            if (obj != null) {
+
+                int counter = 1;
+                System.out.println("\nFirst 10 of Array...");
+
+                for (T i : obj) {
+                    System.out.println(counter + ": " + i.toString());
+
+                    if (counter >= 10) {
+                        break;
+                    }
+                    counter++;
+                }
+
+            } else {
+                System.out.println("Array is null.");
+            }
+        }
+
+        private static <T> T[] copyArray(T[] obj) {
+
+            T[] copy = (T[]) Arrays.copyOf(obj, obj.length);
+
+            return copy;
+        }
+
     }
-
-    private static <T> T[] copyArray(T[] obj) {
-
-        T[] copy = (T[]) Arrays.copyOf(obj, obj.length);
-
-        return copy;
-    }
-
-}
--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -2,34 +2,58 @@ package com.example.sorting;
 
 public class SortingUtility {
 
-
-    public static <T extends Comparable<T>> void gnomeSort(T[] data) {
-
-        // TODO implement Gnome Sort here
+    public static <T extends Comparable<T>> void gnomeSort(T[] a) {
+        int pos = 0;
+        while (pos < a.length) {
+            if (pos == 0 || a[pos].compareTo(a[pos - 1]) >= 0) {
+                pos++;
+            } else {
+                swap(a, pos, pos - 1);
+                pos--;
+            }
+        }
     }
 
-
-    public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
-
-        // TODO implement Cocktail Shaker Sort here
+    public static <T extends Comparable<T>> void cocktailShakerSort(T[] A) {
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i < A.length - 1; i++) {
+                if (A[i].compareTo(A[i + 1]) > 0) {
+                    swap(A, i, i + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+            swapped = false;
+            for (int i = A.length - 2; i >= 0; i--) {
+                if (A[i].compareTo(A[i + 1]) > 0) {
+                    swap(A, i, i + 1);
+                    swapped = true;
+                }
+            }
+        } while (swapped);
     }
 
-
-    public static <T extends Comparable<T>> void shellSort(T[] data) {
-
-        // TODO implement Shell Sort here
+    public static <T extends Comparable<T>> void shellSort(T[] a) {
+        int[] gaps = {711, 342, 123, 64, 24, 11, 5, 1};
+        for (int gap : gaps) {
+            for (int i = gap; i < a.length; i++) {
+                T temp = a[i];
+                int j;
+                for (j = i; j >= gap && a[j - gap].compareTo(temp) > 0; j -= gap) {
+                    a[j] = a[j - gap];
+                }
+                a[j] = temp;
+            }
+        }
     }
 
     private static <T extends Comparable<T>> void swap(T[] data, int index1, int index2) {
-
         T temp = data[index1];
         data[index1] = data[index2];
         data[index2] = temp;
-
     }
 }
-
-
-
-
-
